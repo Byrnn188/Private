89d5c66ff66ce70eae1cf413ae5b00c07baf0dc0
Shuo Wang <3014761073@qq.com>
2022-10-05 13:15:26 +0800

    first commit branch2

--- src/main/java/Park.java
+++ src/main/java/Park.java
@@ -2,4 +2,8 @@ public class Park {
     public static void main(String[] args) {
         System.out.println("Hello world!");
     }
+    public int substract(int x, int y){
+        return(x-y);
+    }
+
 }
